1cb23e6dad9f48c5bf92b9de4ecb56c260e2f591
Michal Vala <mvala@redhat.com>
2020-02-28 08:50:16 +0100

    various improvements

--- cmd/przen.go
+++ cmd/przen.go
@@ -11,13 +11,24 @@ import (
     "os"
 )
 
+type conf struct {
+    username string
+    org      string
+    repo     string
+    prId     int
+    spammer  string
+    token    string
+}
+
 func main() {
     conf := parseArgs()
     client := ghClient(conf)
+    defer printRateLimit(client)
 
-    if pr, response, err := client.PullRequests.Get(context.Background(), conf.org, conf.repo, conf.prId); err != nil {
+    if pr, _, err := client.PullRequests.Get(context.Background(), conf.org, conf.repo, conf.prId); err != nil {
         log.Fatal(err)
     } else {
+        //log.Printf("%+v", *pr.User)
         if *pr.User.Login != conf.username {
             log.Printf("username [%s] and author [%s] names dont match", *pr.User.Login, conf.username)
             log.Fatal("sorry, you can delete only comments on your own PRs")
@@ -25,48 +36,55 @@ func main() {
             log.Printf("usernames matches ok [%s]", conf.username)
         }
 
-        log.Printf("%+v", response)
         //log.Printf("%+v", pr)
         fmt.Printf("[#%d - %s (%s)]\n", *pr.Number, *pr.Title, *pr.User.Login)
         fmt.Printf("[%s]\n", *pr.HTMLURL)
 
-        confirm()
+        confirm(client)
     }
 
-    comments, response, err := client.Issues.ListComments(context.Background(), conf.org, conf.repo, conf.prId, &github.IssueListCommentsOptions{})
+    comments, _, err := client.Issues.ListComments(context.Background(), conf.org, conf.repo, conf.prId, &github.IssueListCommentsOptions{ListOptions: github.ListOptions{PerPage: 100}})
     if err != nil {
         log.Fatal(err)
     }
-    log.Printf("%+v", response)
 
     toDelete := make([]int64, 0)
     for _, comment := range comments {
         //log.Printf("%+v", comment)
-        if *comment.User.Login == conf.spammerUsername {
+        if *comment.User.Login == conf.spammer {
             log.Printf("comment [%d] by [%s] to delete", *comment.ID, *comment.User.Login)
             toDelete = append(toDelete, *comment.ID)
         }
     }
 
     log.Printf("[%d] comments to delete", len(toDelete))
-    confirm()
+    confirm(client)
     for _, commentId := range toDelete {
-        log.Printf("deleting comment [%d]", commentId)
-        if r, deleteErr := client.Issues.DeleteComment(context.Background(), conf.org, conf.repo, commentId); deleteErr != nil {
+        log.Printf("about to delete comment [%d] ...", commentId)
+        if _, deleteErr := client.Issues.DeleteComment(context.Background(), conf.org, conf.repo, commentId); deleteErr != nil {
             log.Fatal(deleteErr)
         } else {
-            log.Printf("%+v", r)
+            log.Print("ok")
         }
     }
 
 }
 
-func confirm() {
+func printRateLimit(client *github.Client) {
+    if rates, _, err := client.RateLimits(context.Background()); err != nil {
+        log.Fatal(err)
+    } else {
+        log.Printf("%+v", rates)
+    }
+}
+
+func confirm(client *github.Client) {
     reader := bufio.NewReader(os.Stdin)
     fmt.Printf("ok? [y/n]: ")
     text, _ := reader.ReadString('\n')
     if text != "y\n" {
-        log.Fatal("interrupted")
+        printRateLimit(client)
+        os.Exit(1)
     }
 }
 
@@ -84,29 +102,36 @@ func ghClient(conf *conf) *github.Client {
 func parseArgs() *conf {
     var conf = &conf{}
 
-    flag.StringVar(&conf.username, "username", "", "your github username")
+    flag.StringVar(&conf.username, "username", "", "your github username, can be set with GITHUB_USERNAME env variable")
     flag.StringVar(&conf.org, "org", "", "name of the orgianization/user of the PR")
     flag.StringVar(&conf.repo, "repo", "", "name of the repo of the PR")
     flag.IntVar(&conf.prId, "prId", 0, "ID of the pull request")
-    flag.StringVar(&conf.spammerUsername, "spammerUsername", "", "username of comments to delete")
-    flag.StringVar(&conf.token, "token", "", "github token")
+    flag.StringVar(&conf.spammer, "spammer", "", "username of comments to delete")
+    flag.StringVar(&conf.token, "token", "", "github token, can be set with GITHUB_TOKEN env var")
 
     flag.Parse()
 
     fmt.Printf("%+v\n", *conf)
 
-    if conf.username == "" || conf.prId == 0 || conf.spammerUsername == "" || conf.token == "" {
+    if conf.username == "" {
+        if username, ok := os.LookupEnv("GITHUB_USERNAME"); ok {
+            conf.username = username
+        } else {
+            log.Fatal("username must be set")
+        }
+    }
+
+    if conf.token == "" {
+        if token, ok := os.LookupEnv("GITHUB_TOKEN"); ok {
+            conf.token = token
+        } else {
+            log.Fatal("github token must be set")
+        }
+    }
+
+    if conf.username == "" || conf.prId == 0 || conf.spammer == "" || conf.token == "" {
         log.Fatal("You must define all params. Use `--help`.")
     }
 
     return conf
-}
-
-type conf struct {
-    username        string
-    org             string
-    repo            string
-    prId            int
-    spammerUsername string
-    token           string
 }
